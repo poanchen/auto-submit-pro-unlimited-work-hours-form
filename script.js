2728c5716173ee629c8c44c78b439980e5ff85bb
Chen <chen.baron@hotmail.com>
2017-04-28 18:04:50 -0700

    updated the script a bit so that it works for Mac, now the time will set for 845am to 445pm with no lunch taken

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if (casper.cli.get('g')) {
   casper.options.logLevel = 'debug';
 }
 
-// dev mode only
+// dev mode to broadcast the console.log error message
 casper.on('remote.message', function(msg) {
   this.echo('remote message caught: ' + msg);
 });
@@ -56,32 +56,42 @@ casper.waitForSelector('select[id="dateRangeString"]', function() {
     // select latest date in data range dropdown
     var dataRangeDropdown = document.querySelector('select[id="dateRangeString"]');
     dataRangeDropdown.selectedIndex = 1;
+
+    // submit the form to get to the edit work hours page
+    document.forms[0].action='/worker/standard/standard_home.html?_finish&_page=0';
+    document.forms[0].submit();
   });
-  // submit the form to get to the edit work hours page
-  this.click('div#addNewDiv a');
 });
 
 casper.then(function() {
   this.waitForUrl(/cntrl_time_create_edit_hourly-4\.html/, function() {
-    this.evaluate(function() {
-      document.querySelector('select[id="billingDetailItems4.billingTimeSpans0.startHourM"]').selectedIndex = 1;
-      document.querySelector('select[id="billingDetailItems4.billingTimeSpans0.endHourM"]').selectedIndex = 2;
-      document.querySelector('input[id="billingDetailItems4.noBreakTaken1"]').checked = true;
-    });
-    // this.fillSelectors('form[method="post"]', {
-    //   'select[id="billingDetailItems4.billingTimeSpans0.startHourM"]': '1',
-    //   'select[id="billingDetailItems4.billingTimeSpans0.endHourM"]': '2',
-    //   'input[id="billingDetailItems4.noBreakTaken1"]': true
-    // }, false);
+    // this.evaluate(function() {
+    //   document.querySelector('select[id="billingDetailItems4.billingTimeSpans0.startHourM"]').selectedIndex = 8;
+    //   document.querySelector('select[id="billingDetailItems4.billingTimeSpans0.startMinute"]').selectedIndex = 45;
+    //   document.querySelector('select[id="billingDetailItems4.billingTimeSpans0.startMeridiem"]').selectedIndex = 1;
+    //   document.querySelector('select[id="billingDetailItems4.billingTimeSpans0.endHourM"]').selectedIndex = 4;
+    //   document.querySelector('select[id="billingDetailItems4.billingTimeSpans0.endMinute"]').selectedIndex = 45;
+    //   document.querySelector('select[id="billingDetailItems4.billingTimeSpans0.endMeridiem"]').selectedIndex = 2;
+    //   document.querySelector('input[id="billingDetailItems4.noBreakTaken1"]').checked = true;
+    // });
+    this.fillSelectors('form[method="post"]', {
+      'select[id="billingDetailItems4.billingTimeSpans0.startHourM"]': '8',
+      'select[id="billingDetailItems4.billingTimeSpans0.startMinute"]': '45',
+      'select[id="billingDetailItems4.billingTimeSpans0.startMeridiem"]': '0',
+      'select[id="billingDetailItems4.billingTimeSpans0.endHourM"]': '4',
+      'select[id="billingDetailItems4.billingTimeSpans0.endMinute"]': '45',
+      'select[id="billingDetailItems4.billingTimeSpans0.endMeridiem"]': '1',
+      'input[id="billingDetailItems4.noBreakTaken1"]': true
+    }, false);
     // submit the edit work hours form
     this.evaluate(function() {
       document.querySelectorAll('input.gray-button')[1].click();
     });
     // this is not the best way of doing it
-    this.wait(2000, function() {
+    this.waitForUrl(/\?\_page=0\&\_target2/, function() {
       this.viewport(1000, 1080);
       this.capture('./screenshot.png', {top: 0,left: 0,width: 1000, height: 2000});
-    })
+    });
   });
 });
 
